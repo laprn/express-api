c8775f5901ebcdcc48f2bd73941b8f539b893eed
laprn <reg@dymcom.net>
2021-01-27 18:44:10 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); 
+
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
+
+app.get('/', (req, res) => {
+    res.send('Hello express');
+});
+
+mongoose.connect(
+    process.env.DB_CONNECTION, {useNewUrlParser: true}, () => {
+    console.log('connected to DB!')
+});
+
+app.listen(3000);
